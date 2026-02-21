6ee8d5f011d548301a7e94de768909acbc75a255
Pratik Dubey <pratikdubey091@gmail.com>
2026-02-21 22:27:31 +0530

    setted up neon-postgres

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const appfont = DM_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  // weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={appfont.className}
       >
         {children}
       </body>
---
+++ config/schema.tsx
@@ -0,0 +1,8 @@
+import { pgTable, serial, varchar, integer } from 'drizzle-orm/pg-core';
+
+export const users = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    credits: integer().default(5)
+})
